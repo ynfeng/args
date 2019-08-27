8ea3fb300af62724fae5272f55547e78f2b8ba2e
FengYannan <fengyannan@mintechai.com>
2019-08-27 17:24:16 +0800

    Default Changelist

--- src/test/java/com/github/ynfeng/args/ArgsTest.java
+++ src/test/java/com/github/ynfeng/args/ArgsTest.java
@@ -20,7 +20,7 @@ public class ArgsTest {
     @Test
     public void should_get_defalut_value() {
         Args args = new Args(Schema.of("l:boolean:false p:int:8080"), ArgLine.of(""));
-//        assertThat(args.argValue("l"), is(false));
+        assertThat(args.argValue("l"), is(false));
         assertThat(args.argValue("p"), is(0));
     }
 
